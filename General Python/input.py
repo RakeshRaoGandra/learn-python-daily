622dbb9c755eec9cae05a60607e685ad8c8de224
Rakesh Rao Gandra <gandrarakeshrao123@gmail.com>
2026-08-16 18:29:16 +0530

    Day second of learning python from scarch

--- General Python/input.py
+++ General Python/input.py
@@ -1,6 +1,7 @@
 # Today Date 15/08/2026
 # Topic : How to take input in python
 # Single value INput
+'''
 s=input("Enter a value:")
 print(s)
 
@@ -11,5 +12,54 @@ a, b = map(int, input("Enter two numbers: ").split())
 
 nums = list(map(int, input("Enter numbers: ").split()))
 print(nums)
+'''
+#  It is for the string  and number also
+name=input("Enter Your name : ")
+print(name)
+
+# Basic syntax for input
+# variable = input("message")
+
+age = input("Enter your age: ")
+
+print(age)
+
+#  VERY IMPORTANT: input() returns a string
+
+age = input("Enter your age: ")
+
+print(type(age))
+
+# it reurns like "12" like in out put it is a string so dont do 
+
+# if we 
+#  Converting user input to an integer
+age = int(input("Enter your age: "))
+
+print(age)
 
 
+#  for Getting a decimal number
+
+
+price = float(input("Enter price: "))
+
+print(price)
+
+
+## Multiple inputs 
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+salary = float(input("Enter salary: "))
+
+print(name)
+print(age)
+print(salary)
+
+### int() vs float()
+
+age = int(input("Age: "))
+quantity = int(input("Quantity: "))
+
+price = float(input("Price: "))
+salary = float(input("Salary: "))
---
+++ General Python/variables.py
@@ -0,0 +1,55 @@
+# Variables 
+# A variable is a name used to store a value in Python.
+
+# Examples
+age = 21
+name = "Shadow"
+salary = 35000
+
+print(age)
+print(name)
+print(salary)
+
+#        The basic syntax is:
+ #     variable_name = value
+
+name = "Shadow"       # string
+age = 21              # integer
+marks = 85.5          # float
+is_student = True     # boolean
+
+print(age)
+print(type(age))
+print(name)
+print(type(name))
+print(marks)
+print(type(marks))
+print(is_student)
+print(type(is_student))
+
+
+# Multiple variables
+
+name = "Rahul"
+age = 21
+city = "Hyderabad"
+# Like  these 
+name, age, city = "Rahul", 21, "Hyderabad"
+
+print(name)
+print(age)
+print(city)
+
+# Assigning the same value
+
+x = y = z = 100
+print(x)
+print(y)   # sse the output al values are same
+print(z)
+
+##
+#  Important difference: = vs ==
+
+age = 21 #  means assignment
+age == 21  # means comparison
+
