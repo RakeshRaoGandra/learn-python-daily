31c943e8b1ee80d38eeec6dc31946a820dfac16f
Rakesh Rao Gandra <gandrarakeshrao123@gmail.com>
2026-08-29 21:35:11 +0530

    this is the string file

---
+++ strings.py
@@ -0,0 +1,25 @@
+# Strings
+s="rakesh rao"
+print(s)
+print(type(s))
+print(s[-7])
+print(len(s))
+#start end 
+print(s[0:3])
+print(s[3:])
+print(s[::])
+print(s[:5])
+#start end step
+print(s[3:2])
+# reapating strings
+print("Hi" * 3)
+print("-" * 10)
+# checking characters with in 
+
+word = "Python"
+
+print("P" in word)
+print("the" in word)
+word = "Python"
+
+print("Pee" not in word)
